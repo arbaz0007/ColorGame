526be59f41c308a5ae8074112e66f68e83662a2e
arbaz0007 <arbazk.ak007@gmail.com>
2023-07-22 04:01:55 +0530

    second commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Button } from "react-native"
-import {View,Text,SafeAreaView} from "react-native"
+import {View,Text,SafeAreaView,StyleSheet} from "react-native"
 const App =() =>{
   const [bgColor,setBgColor] = useState(`rgb(127,0,255)`)
   const onPressHandler =() => {
@@ -8,12 +8,16 @@ const App =() =>{
     setBgColor(randomColor)
   }
 return<SafeAreaView style={{
-  flex:1,
-  justifyContent:"center",
-  alignItems:"center",
-  backgroundColor:bgColor
+  
 }}> 
 <Button title ="click me" onPress={onPressHandler}/>
 </SafeAreaView>
 }
 export default App
+const style =StyleSheet.create
+container:{
+  flex:1,
+  justifyContent:"center",
+  alignItems:"center",
+  backgroundColor:bgColor
+}
