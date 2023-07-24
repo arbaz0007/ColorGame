04bdd68a6fc122a5e16f627030ec0fcf2c91e927
arbaz0007 <arbazk.ak007@gmail.com>
2023-07-24 16:30:59 +0530

    third commit

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import { useState } from "react"
-import { Button } from "react-native"
-import {View,Text,SafeAreaView,StyleSheet} from "react-native"
-const App =() =>{
-  const [bgColor,setBgColor] = useState(`rgb(127,0,255)`)
+import { Pressable  } from "react-native"
+import {Text,SafeAreaView,StyleSheet} from "react-native"
+const App = () =>{
+  const [bgColor,setBgColor] = useState(`rgba(127,0,255)`)
   const onPressHandler =() => {
     const randomColor =`rgb(${Math.floor(Math.random()*256)} , ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`
     setBgColor(randomColor)
   }
-return<SafeAreaView style={{
-  <Text  style= {styles.text}> Press Me</Text>
+  
+  const onPressHandler2 =() => {
+    
+    setBgColor(`rgba(0,0,0)`)
+  }
+return<SafeAreaView style={[styles.container,{backgroundColor:bgColor}]} >
+ <Pressable onPress={onPressHandler}> <Text  style= {styles.text}> Press Me</Text></Pressable>
+ <Pressable onPress={onPressHandler2}> <Text  style= {styles.text2}> Press Me</Text></Pressable>
 </SafeAreaView>
-}}> 
+
 
 }
 export default App
-const style =StyleSheet.create
+const styles =StyleSheet.create({
 container:{
   flex:1, 
   justifyContent:"center",
   alignItems : "center",
-  backgroundColor:bgColor
+  backgroundColor:rgba(127,0,255)
 },
 text:{
   backgroundColor :"#ffffff",
@@ -28,6 +34,18 @@ text:{
   paddingVerticle:20,
   fontSize:20,
   borderRadius:20,
-  backgroundColor:'rgb(127,0,255)',
+  backgroundColor:`rgba(127,0,255)`,
   color:"#fff"
-}
+},
+text2:{
+  backgroundColor :"#ffffff",
+  padding:20,
+  marginVertical:20,
+  paddingHorizontal:10,
+  paddingVerticle:20,
+  fontSize:20,
+  borderRadius:20,
+  backgroundColor:'rgba(127,0,255)',
+  color:"#fff"
+} 
+})
